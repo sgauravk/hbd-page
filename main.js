7d57b9dc11876eb1514d0659022dad982e4b8a12
Gaurav Saini <44020369+sgauravk@users.noreply.github.com>
2019-05-06 11:32:11 +0530

    introduce html page

---
+++ main.js
@@ -0,0 +1,23 @@
+let count = 1;
+
+const dashGenerator = count => new Array(count).fill(".").join('');
+
+const startCounting = function() {
+  const _ = dashGenerator(count);
+  document.getElementById("waiting-text").innerText = "press it again" + _;
+  if (count++ < 10) return;
+  displayMsg();
+};
+
+const displayMsg = function() {
+  const body = document.getElementById("body");
+  const name = document.getElementById("name").value.toUpperCase();
+  const msg = "HAPPY BIRTHDAY " + name;
+  const msgDiv = document.createElement("div");
+  msgDiv.className = "msg-text";
+  msgDiv.innerText = msg;
+  body.innerHTML = "";
+  body.appendChild(msgDiv);
+  body.style.background = 'url("./hbd.gif")';
+  document.getElementById("quote").style.visibility = "visible";
+};
